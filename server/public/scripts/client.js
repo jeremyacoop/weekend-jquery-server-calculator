4728e3e0837922e4435b3e35f3ce8b80b0e2c713
jeremyacoop <63517273+jeremyacoop@users.noreply.github.com>
2021-12-21 20:16:31 -0600

    Set up inputs and buttons, testing click handlers

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,5 +4,11 @@ $(document).ready(handleReady);
 
 function handleReady() {
     console.log('jQuery is ready');
+    $('#calc-solve').on('click', function() {
+        console.log('equals button');
+    })
+    $('#calc-clear').on('click', function() {
+        console.log('clear button');
+    })
     }
 
