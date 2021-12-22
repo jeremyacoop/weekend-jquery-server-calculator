c70dd09274b602cb7effdb5fa2d097ca3626953b
jeremyacoop <63517273+jeremyacoop@users.noreply.github.com>
2021-12-21 22:40:51 -0600

    Wrote collectData function and (working!) operator input

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,13 +2,32 @@ console.log('js');
 
 $(document).ready(handleReady);
 
+let numOperator = '';
+
 function handleReady() {
     console.log('jQuery is ready');
-    $('#calc-solve').on('click', function() {
-        console.log('equals button');
-    })
+    $('.number-operator').on('click', getOperator);
+    //$('#calc-subtract').on('click', getOperator);
+    $('#calc-solve').on('click', collectData);
     $('#calc-clear').on('click', function() {
         console.log('clear button');
     })
-    }
+}
 
+function getOperator() {
+    console.log('in getOperator');
+    numOperator = $(this).text();
+    console.log('operator: ', numOperator)
+}
+
+function collectData() {
+    console.log('in collectData');
+    console.log($('#first-number').val());
+    console.log($('#second-number').val());
+    //console.log($('#calc-add').val());
+    //console.log($('#calc-subtract').val());
+    //console.log($('#calc-multiply').val());
+    //console.log($('#calc-divide').val());
+    console.log($('.number-operator').val());
+    console.log(numOperator);
+}
