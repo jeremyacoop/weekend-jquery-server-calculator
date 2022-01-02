4af7669e58336f7969dd5c35272d08c6e4c783fd
jeremyacoop <63517273+jeremyacoop@users.noreply.github.com>
2022-01-01 18:51:29 -0600

    Wrote sendMathProblem function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -8,7 +8,7 @@ function handleReady() {
     console.log('jQuery is ready');
     $('.number-operator').on('click', getOperator);
     //$('#calc-subtract').on('click', getOperator);
-    $('#calc-solve').on('click', collectData);
+    $('#calc-solve').on('click', sendMathProblem);
     $('#calc-clear').on('click', function() {
         console.log('clear button');
     })
@@ -22,19 +22,19 @@ function getOperator() {
 
 function collectData() {
     console.log('in collectData');
-    console.log($('#first-number').val());
-    console.log($('#second-number').val());
+    //console.log($('#first-number').val());
+    //console.log($('#second-number').val());
     //console.log($('#calc-add').val());
     //console.log($('#calc-subtract').val());
     //console.log($('#calc-multiply').val());
     //console.log($('#calc-divide').val());
-    console.log($('.number-operator').val());
+    //console.log($('.number-operator').val());
     console.log(numOperator);
 
     let numOne = $('#first-number').val();
-    console.log(numOne);
+    //console.log(numOne);
     let numTwo = $('#second-number').val();
-    console.log(numTwo);
+    //console.log(numTwo);
 
     let numData = {
         numberOne: numOne,
@@ -44,4 +44,24 @@ function collectData() {
     }
     console.log(numData);
     return numData;
+}
+
+function sendMathProblem(problemToSend) {
+    console.log('in sendMathProblem');
+    console.log(problemToSend);
+    problemToSend = collectData();
+    console.log(problemToSend);
+    $.ajax({
+        method: 'POST',
+        url:    '/calculator',
+        data:   problemToSend
+    }).then( function(response) {
+        getMathSolution();
+    }).catch(function(err) {
+        alert('Server unable to respond');
+    });
+}
+
+function getMathSolution() {
+    console.log('In getMathSolution');
 }
